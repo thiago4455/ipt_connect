6b086070e64942cf9abe08e63eed1fc7e31f02fa
Thiago Mattos <thiago.mattos@tradexsolutions.com.br>
2024-11-01 23:03:27 -0300

    Upgrade tp python 3

--- ipt_connect/ipt_connect/settings.py
+++ ipt_connect/ipt_connect/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'loginas',
 ) + INSTALLED_TOURNAMENTS
 
 MIGRATION_MODULES = dict(
