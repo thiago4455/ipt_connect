e402ea8371d63a576a54157404abe3aacc681c7f
Thiago Mattos <thiago.mattos@tradexsolutions.com.br>
2024-11-02 14:04:06 -0300

    Remove external link from header

--- ipt_connect/ipt_connect/views.py
+++ ipt_connect/ipt_connect/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def home(request):
     text = """<h1>IPT Connect</h1>
 
-              <a href="http://connect.iptnet.info/BPT">Results of BPT dev</a>"""
+              <a href="/">Results of BPT dev</a>"""
     return HttpResponse(text)
